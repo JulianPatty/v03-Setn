4d65cda148ff8b4f1cfd7b8afb4f4687e1b4f26b
Julian Patrick <help@setn.ai>
2025-06-30 14:49:42 -0400

    Center the initial node on screen

--- src/data/workflow-data.ts
+++ src/data/workflow-data.ts
@@ -2,7 +2,11 @@ import { AppEdge, createEdge } from '@/components/edges';
 import { AppNode, createNodeByType } from '@/components/nodes';
 
 export const initialNodes: AppNode[] = [
-  createNodeByType({ type: 'initial-node', id: 'workflowNode_1' }),
+  createNodeByType({ 
+    type: 'initial-node', 
+    id: 'workflowNode_1',
+    position: { x: 0, y: 0 } // This will be centered by the createNodeByType function
+  }),
 ];
 
 export const initialEdges: AppEdge[] = [];
